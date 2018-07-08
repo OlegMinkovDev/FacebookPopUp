9a44d468838518f9aa0cfa63f5d45f35d7681288
Minkov Oleg <oleg.minkov93@gmail.com>
2018-07-08 13:15:45 +0300

    Initial Commit

---
+++ FacebookPopUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookPopUp
+//
+//  Created by Oleg Minkov on 7/8/18.
+//  Copyright © 2018 Oleg Minkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
